d026e0741a028c2bee2c394a65fa831a7649ad61
BrilAlex <brilalex.it@yandex.ru>
2022-10-20 13:24:06 +0300

    Refactor unsubscribe queryParams subscription on pokemon list page

--- src/app/features/pokemon-list/components/pokemon-list.component.ts
+++ src/app/features/pokemon-list/components/pokemon-list.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit} from "@angular/core";
 import {PokemonListService} from "../services/pokemon-list.service";
 import {PokemonListData} from "../models/pokemon-list.models";
-import {Observable, Subscription} from "rxjs";
+import {finalize, Observable, Subject, Subscription, takeUntil} from "rxjs";
 import {AppService} from "../../../core/services/app.service";
 import {RequestStatus} from "../../../core/models/app.models";
 import {ActivatedRoute, Params, Router} from "@angular/router";
@@ -17,7 +17,7 @@ import {ActivatedRoute, Params, Router} from "@angular/router";
 export class PokemonListComponent implements OnInit, OnDestroy {
   appStatus$!: Observable<RequestStatus>;
   pokemonListData$!: Observable<PokemonListData>;
-  queryParamsSubscription!: Subscription;
+  destroySubject = new Subject<void>();
 
   constructor(
     private pokemonListService: PokemonListService,
@@ -29,18 +29,24 @@ export class PokemonListComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.appStatus$ = this.appService.appStatus$;
-    this.queryParamsSubscription = this.route.queryParams.subscribe((params: Params) => {
-      const limit = Number(params["limit"]) ? Number(params["limit"]) : 20;
-      const offset = Number(params["offset"]) ? Number(params["offset"]) : 0;
-      this.getPokemonList({limit, offset});
-    });
+    this.route.queryParams
+      .pipe(
+        finalize(() => console.log("unsubscribe")),
+        takeUntil(this.destroySubject),
+      )
+      .subscribe((params: Params) => {
+        const limit = Number(params["limit"]) ? Number(params["limit"]) : 20;
+        const offset = Number(params["offset"]) ? Number(params["offset"]) : 0;
+        this.getPokemonList({limit, offset});
+      });
   };
 
   ngOnDestroy() {
-    this.queryParamsSubscription.unsubscribe();
+    this.destroySubject.next();
+    this.destroySubject.complete();
   };
 
-  getPokemonList(params: {limit: number, offset: number}) {
+  getPokemonList(params: { limit: number, offset: number }) {
     this.pokemonListService.getPokemonListData(params);
     this.pokemonListData$ = this.pokemonListService.pokemonListData$;
   };
